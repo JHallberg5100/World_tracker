de67bc21729505c042ebe7d594767188b3979d2a
JHallberg5100 <JHallberg5100@gmail.com>
2016-11-02 08:43:53 -0500

    Begin loremaster

---
+++ loremaster.rb
@@ -0,0 +1,15 @@
+#Create methods
+
+
+
+
+#Initialize Loremaster and prompt for direction
+puts "Hello and welcome to Loremaster v1.0"
+puts "What are you looking for information about?"
+input = gets.chomp
+if input == "world"
+  lore()
+elsif input == "quests"
+else
+
+end
